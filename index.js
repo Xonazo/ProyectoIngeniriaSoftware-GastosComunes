ce4e7f2a79ed398485a02fe5e3b7bd91407310c8
Nelson Rubio <nelson.rubio1901@alumnos.ubiobio.cl>
2022-11-13 00:40:31 -0300

    Registros

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-const express= require('express');
-//const mongoose = require('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const app = express();
 dotenv.config();
 
-
+//a
 const mailerRoutes = require('./Routes/mailerRoutes')
-app.use('/api',mailerRoutes);
+const vecinoRoutes = require('./Routes/vecinoRoutes')
+const RegistroRoutes = require('./Routes/RegistroRoutes')
 
+app.use(cors());
+app.use(express.json());
+app.options('*', cors());
 
-const options ={
+app.use('/api', mailerRoutes);
+app.use('/api', vecinoRoutes);
+app.use('/api', RegistroRoutes);
+
+const options = {
     useNewUrlParser: true,
     autoIndex: true,
     keepAlive: true,
-    connectTimeoutMS:10000,
+    connectTimeoutMS: 10000,
     socketTimeoutMS: 45000,
     family: 4,
     useUnifiedTopology: true
 }
- 
 
-app.listen(process.env.PORT,()=>
-{
-console.log(`Server started on port ${process.env.PORT}`);
+
+mongoose.connect(process.env.DB, options, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Connected to database")
+    }
+
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
 })
