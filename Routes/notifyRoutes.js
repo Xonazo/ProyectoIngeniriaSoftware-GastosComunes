68746819501765cad2db971e9cdd0d5b7660c27f
Matias San Martin M <98131860+lmxti@users.noreply.github.com>
2022-11-25 00:14:06 -0300

    Delete Routes directory

--- Routes/notifyRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const notifyController = require('../Controller/notifyController');
-const api = express.Router();
-
-api.get('/notify', notifyController.notify);
-
-
-module.exports = api
