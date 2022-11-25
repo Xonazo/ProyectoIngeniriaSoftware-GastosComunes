8c9d6eaadfc9722f740e56ac4e67a54acd14a321
DJVegaG <domingo.vega1901@alumnos.ubiobio.cl>
2022-11-24 23:15:00 -0300

    arreglo 10

--- Routes/fileRoutes.js
+++ Routes/fileRoutes.js
@@ -6,5 +6,7 @@ const fileSize = require('../middlewares/fileSize')
 const api = express.Router()
 
 api.post("/file/:archivo", upload.array('archivos'),fileSize,fileController.uploadNewFile)
+api.get('/files', fileController.getFiles)
+api.get('/file/download/:id',fileController.getSpecificFile)
 
 module.exports = api
