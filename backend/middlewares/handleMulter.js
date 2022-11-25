f68d6eaed34ea8201e397c64fb0cdf440eb42115
DJVegaG <domingo.vega1901@alumnos.ubiobio.cl>
2022-11-25 16:18:53 -0300

    16:18

--- backend/middlewares/handleMulter.js
+++ backend/middlewares/handleMulter.js
@@ -20,7 +20,7 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage: storage,
     fileFilter: function (req, file, cb) {
-        if (file.mimetype == 'image/png') {
+        if (file.mimetype === 'image/png') {
             console.log("El archivo es un png")
         } else {
             console.log("El archivo tiene otra extension")
