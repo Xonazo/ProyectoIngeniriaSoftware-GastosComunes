0c918c36e570caa7663ebd47c9a0044196a92a8b
DJVegaG <domingo.vega1901@alumnos.ubiobio.cl>
2022-12-13 21:24:01 -0300

    Cosas main

---
+++ frontend/components/LoginForm.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import avatar from "../public/avatar.svg";
+
+const LoginForm = () => {
+  return (
+    <section className='container'>
+      <div className='box'>
+        <div className='form'>
+            <h2>Sig in</h2>
+            <div className='inputBox'>
+              <input type="text" required="required" />
+              <span>Username</span>
+              <i></i>
+            </div>
+
+            <div className='inputBox'>
+              <input type="password" required="required" />
+              <span>Password</span>
+              <i></i>
+            </div>
+
+            <div className='links'>
+              <a href="#">Forgot password</a>
+              <a href="#">Signup</a>
+            </div>
+
+            <input type="submit" value="Login"/>
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default LoginForm
+
---
+++ frontend/pages/signin.js
@@ -0,0 +1,13 @@
+import LoginForm from '../components/LoginForm';
+import  NavBar  from '../components/NavBar';
+
+
+export default function Home() {
+
+  return (
+    <div>
+      <NavBar/>
+      <LoginForm/>
+    </div>
+  )
+}
